d9ab9c4e25e147dd4f005e946257f4ec69aadac3
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2024-11-21 15:09:08 +0100

    Styles for image

--- src/components/CharacterWidget/CharacterWidget.styled.ts
+++ src/components/CharacterWidget/CharacterWidget.styled.ts
@@ -2,7 +2,6 @@ import styled from "@emotion/styled";
 
 export const CharacterWidgetContainer = styled.div`
   width: 300px;
-  height: 100px;
   border-radius: 4px;
   border: 1px solid darkgray;
   font-size: 11px;
@@ -13,11 +12,10 @@ export const CharacterStatusLabel = styled.span<{ isAlive: boolean }>`
 `;
 
 export const CharacterAvatar = styled.img`
-  box-sizing: border-box;
   width: 70px;
   height: 70px;
-  border-radius: 3px;
-  border: 2px solid hotpink;
+  border-radius: 4px;
+  filter: drop-shadow(0 4px 4px #00000026);
 `;
 
 export const Heading = styled.div`
@@ -33,5 +31,6 @@ export const CharacterName = styled.h4`
 export const Content = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-between;
+  padding: 10px;
 `;

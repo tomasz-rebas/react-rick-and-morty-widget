19ecd60233c8283143101850fa522d666baea67e
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2024-11-21 16:15:44 +0100

    Prepare button components

---
+++ src/components/Controls/Controls.styled.ts
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  margin-top: 20px;
+`;
+
+export const Button = styled.button`
+  width: 90px;
+  height: 30px;
+  font-size: 12px;
+  font-weight: 700;
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  background-color: #f2f2f2;
+  text-decoration: none;
+  cursor: pointer;
+
+  :hover {
+    background-color: #e8e8e8;
+  }
+
+  :active {
+    transform: translateY(2px);
+  }
+`;
---
+++ src/components/Controls/Controls.tsx
@@ -0,0 +1,12 @@
+import { Button, Wrapper } from "./Controls.styled";
+
+const Controls: React.FC = () => {
+  return (
+    <Wrapper>
+      <Button>Previous</Button>
+      <Button>Next</Button>
+    </Wrapper>
+  );
+};
+
+export default Controls;
